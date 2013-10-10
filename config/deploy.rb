857de2d6eb8b3d5ff5b66a7f6a9fe4f0123b18c4
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2013-10-10 12:51:42 +0300

    Task for creating initial user

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 
+require 'bundler/capistrano'
 require 'sidekiq/capistrano'
-
+require 'puma/capistrano'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -27,6 +28,10 @@ set :use_sudo, false
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+set :default_environment, {
+  'PATH' => "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
+}
 
 namespace :deploy do
   task :migrate do; end
---
+++ lib/tasks/seed.rake
@@ -0,0 +1,11 @@
+namespace :users do
+  desc 'Creates initial users'
+  task :create_first_user do
+    unless User.where(name: 'admin').first
+      User.create(name:                  'admin',
+                  email:                 'admin@gt2.ru',
+                  password:              'gt2rulez',
+                  password_confirmation: 'gt2rulez')
+    end
+  end
+end
