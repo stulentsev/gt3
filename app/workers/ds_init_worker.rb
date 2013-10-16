59c4e439140a6ec3bef4c0d41e7bc958968f038f
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2013-10-16 12:28:07 +0300

    Fix bug

--- app/workers/ds_init_worker.rb
+++ app/workers/ds_init_worker.rb
@@ -8,12 +8,12 @@ class DsInitWorker
     app_id = args.fetch(:app_id)
     time   = args.fetch(:time)
 
-    doc_id, updates = prepare_update_opts(app_id, time, val)
+    doc_id, updates = prepare_update_opts(app_id, time)
     DailyStat.update_stats(doc_id, updates)
   end
 
   private
-  def prepare_update_opts(app_id, time, val)
+  def prepare_update_opts(app_id, time)
     id = "#{app_id}_#{time.compact}"
 
     defaults = DailyStat.new.attributes.except('_id')
